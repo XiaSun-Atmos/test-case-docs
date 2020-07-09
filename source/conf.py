aec8603852e0b2e01f77a5fbee1b8cb404b74410
Xia Sun <xia.sun@noaa.gov>
2020-07-09 17:18:33 -0600

    add conf.py

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020 '
 author = 'Xia Sun'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
